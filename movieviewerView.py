840abbe1b7eefab054f06ba6229fa92d315c54b8
harit12 <hmetla1245@gmail.com>
2020-11-15 16:40:36 -0500

    Delete movieviewerView.py

--- movieviewerView.py
+++
@@ -1,7 +0,0 @@
-class View:
-    def initial(self):
-        print("Type review:")
-    def displayResults(self, pos, neg, res):
-        print("The probability of review being positive is: ", pos)
-        print("The probability of review being negative is: ", neg)
-        print("Therefore, the review is ", res)
