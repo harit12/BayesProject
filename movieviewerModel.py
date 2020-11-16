63bac33e66cf522187c978b8ac56974e11934f06
harit12 <hmetla1245@gmail.com>
2020-11-16 12:19:55 -0500

    Compress pretrained model

--- movieviewerModel.py
+++ movieviewerModel.py
@@ -9,7 +9,8 @@ import spacy
 from collections import Counter
 #Debugging
 import datetime
-
+#Store Pretrained
+import pickle
 nlp = spacy.load('en_core_web_sm')
 
 #Debugging:
@@ -294,6 +295,6 @@ def main():
     pass
 if __name__ =='__main__':
     main()
-
+    
 
     
